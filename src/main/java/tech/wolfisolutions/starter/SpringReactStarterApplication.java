365d50e12371ea1cc677bd6e94ebfd7ad510a6dc
Catlin Cox <catlin.cox@wolfisolutions.tech>
2022-03-22 18:35:07 -0500

    [WOLFI-1] - Template Created
    This is a basic template for creating a spring boot with react typescript application. It can further be expanded upon.

---
+++ src/main/java/tech/wolfisolutions/starter/SpringReactStarterApplication.java
@@ -0,0 +1,12 @@
+package tech.wolfisolutions.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactStarterApplication.class, args);
+    }
+}
